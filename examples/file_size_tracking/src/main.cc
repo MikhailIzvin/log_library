302016ade6b7f9990fa67b0e1f24c76a5c0ba4a5
Mikhail Izvin <mai@tvip.ru>
2024-11-08 17:41:10 +0300

    Add support for custom log directory and implement file size tracking example

---
+++ examples/file_size_tracking/src/main.cc
@@ -0,0 +1,61 @@
+#include "config.h"
+#include "logger.h"
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+#define MAX_THREADS 10
+#define MAX_LOG_SIZE 1000000
+static int logFileCounter = 0;
+
+namespace fs = std::filesystem;
+
+void maxFileSizeCallback(void *userdata) {
+  fs::path *log_dir = (fs::path *) userdata;
+  logFileCounter++;
+  fs::path logFile = *log_dir / fs::path("log." + std::to_string(logFileCounter) + ".txt");
+
+  // You should unlock before using library functions
+  LOG_LIBRARY_UNLOCK();
+  log_library_set_log_file(logFile.c_str());
+}
+
+int main() {
+  fs::path cmake_log_dir(LOG_DIR);
+  fs::path log_dir = cmake_log_dir / fs::path("log");
+  if (!fs::exists(log_dir)) {
+    fs::create_directory(log_dir);
+  }
+
+  fs::path logFile = log_dir / fs::path("log." + std::to_string(logFileCounter) + ".txt");
+
+  log_library_set_log_file(logFile.c_str());
+  log_library_set_log_max_size(MAX_LOG_SIZE);
+  log_library_set_max_file_size_callback(maxFileSizeCallback, (void *) &log_dir);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  std::vector<std::thread> threads;
+  for (int i = 0; i < MAX_THREADS; i++) {
+    threads.push_back(std::thread([&]() {
+      for (int j = 0; j < 10000; j++) {
+        std::thread::id this_id = std::this_thread::get_id();
+
+        LOGDEBUG("Thread %d started with id %d", i, this_id);
+        LOGINFO("Thread %d started with id %d", i, this_id);
+        LOGERROR("Thread %d started with id %d", i, this_id);
+        LOGWARN("Thread %d started with id %d", i, this_id);
+      }
+    }));
+  }
+
+  for (auto &thread: threads) {
+    thread.join();
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end - start;
+  std::cout << "Elapsed time: " << elapsed.count() << "s\n";
+}
